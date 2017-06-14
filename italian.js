a9167824df8b10b812de36b98b635ef2edd811f5
Greta Moseley <greta.moseley@gmail.com>
2017-06-14 17:52:09 -0500

    add breaking object into two arrays, iterating over arrays in for loop

--- italian.js
+++ italian.js
@@ -1,20 +1,35 @@
-let translator = (function(object){
+var translator = (function(global){
 
 //private variable
 let italian ={merry:"buon", christmas:"natale", and:"e", happy:"felice", new:"nuovo", year:"anno"};
 
+//turn this object into 2 equal length arrays
+let englishArray =[];
+
+for(var key in italian){
+	englishArray.push(key);
+}
+
+let italianArray=[];
+for (var value in italian){
+	italianArray.push(italian[value]);
+}
+
+console.log ("italianArray",italianArray);
+console.log ("englishArray",englishArray);
 
 var italianTranslation= "";
 
-object.translateToItalian=function(inputString){
-for(key in italian){
+global.translateToItalian=function(inputString){
+	for(var i=0; i<englishArray.length; i++){
+		italianTranslation += inputString.toLowerCase.replace(/englishArray[i]/g, italianArray[i] );
 
- italianTranslation += inputString.toLowerCase.replace(/merry/g, "Buon" );
+		}
 
+ 	return italianTranslation;
 }
 
-	return italianTranslation;
-}
+ return global;
 
-return object;
-}(translator));
+ }(translator));
+

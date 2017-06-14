0aad8b04c93da34115c614fb50089fe55336e757
Hayley Landsberg <hayleyheath@Hayleys-MacBook-Air.local>
2017-06-14 15:51:39 -0500

    Added html file with skeleton and german js file with global variable and lexicon

---
+++ german.js
@@ -0,0 +1,18 @@
+var translator = (function(global) {
+	let german = {
+		merry : "frohe",
+		christmas : "weihnachten",
+		and : "und",
+		happy : "ein glückliches",
+		new : "neues",
+		year : "jahr"
+	};
+
+	global.translateToGerman = function(inputString) {
+		inputString.replace()
+		return translateToGerman;
+	}
+
+	return global;
+
+}(translator));

b78821264a7ca553da46651ee85521953fd3e170
Greta Moseley <greta.moseley@gmail.com>
2017-06-14 15:55:19 -0500

    add base italian.js file and main.js with empty object

---
+++ italian.js
@@ -0,0 +1,20 @@
+let translator = (function(object){
+
+//private variable
+let italian ={merry:"buon", christmas:"natale", and:"e", happy:"felice", new:"nuovo", year:"anno"};
+
+
+var italianTranslation= "";
+
+object.translateToItalian=function(inputString){
+for(key in italian){
+
+ italianTranslation += inputString.toLowerCase.replace(/merry/g, "Buon" );
+
+}
+
+	return italianTranslation;
+}
+
+return object;
+}(translator));
---
+++ main.js
@@ -0,0 +1,2 @@
+//global variable with empty object
+let translator = {};

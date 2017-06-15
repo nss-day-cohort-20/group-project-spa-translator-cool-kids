025695ab15644dfc0337d5e2f3ad395b552e6032
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-06-14 23:55:27 -0500

    added rough draft for event handler and linked in html

---
+++ events.js
@@ -0,0 +1,24 @@
+//var events = (function() {
+
+let btn = document.getElementById('translateBtn');
+let inputText = document.getElementById('inputText');
+let outputText = document.getElementById('output');
+let languageSelection = document.getElementById('languageChoice');
+
+function translateHandler(clickEvent) {
+	console.log(clickEvent);
+	if (languageSelection === 'German') {
+		outputText.innerHTML = translator.translateToGerman(inputText.value);
+	} else if (languageSelection === 'Italian') {
+		outputText.innerHTML = translator.translateToItalian(inputText.value);
+	} else if (languageSelection === 'Spanish') {
+		outputText.innerHTML = translator.translateToSpanish(inputText.value);
+	} else {
+		alert('Please select a language and/or type a christmas greeting');
+	}
+}
+
+btn.addEventListener('click', translateHandler);
+
+//}());
+
